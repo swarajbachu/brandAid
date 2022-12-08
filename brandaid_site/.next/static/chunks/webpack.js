3e3f6cb44e70767e5553502cdcb29d7fd629fb22
swaraj bachu <rajswaraj.r@gmail.com>
2022-12-08 14:45:40 +0530

    added front end

--- brandaid_site/.next/static/chunks/webpack.js
+++ brandaid_site/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e1df97c44413ffc1"; }
+/******/ 		__webpack_require__.h = function() { return "85c17b71dcd67853"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- brandaid_site/.next/static/webpack/webpack.a4a0284e0d2195d2.hot-update.js
+++ brandaid_site/.next/static/webpack/webpack.a4a0284e0d2195d2.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8d3e7e23ab3f9101"; }
+/******/ 	__webpack_require__.h = function() { return "85c17b71dcd67853"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
